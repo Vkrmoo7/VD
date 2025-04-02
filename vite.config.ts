bb69fe02eb211450f38b7bbf9aeb2bd2e84c0ea2
vkrm <vkrm@localhost.localdomain>
2025-04-02 07:20:18 +0530

    Initial project upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/smartvd-bmi-calculator/', // Your repository name here
+  base: "/VD/" // Repository name
 });
